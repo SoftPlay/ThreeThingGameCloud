5b90d2e1336ea43471b0e68ec61defb1c274a369
Aidan Treasure <aidan.treasure@apdcomms.co.uk>
2017-05-12 22:29:08 +0100

    Done some stuff. It's 10:30 pm :(

--- GameState/run.fsx
+++ GameState/run.fsx
@@ -1,21 +1,58 @@
+#if !COMPILED
+#I @"C:\Users\aidan.treasure\AppData\Roaming\npm\node_modules\azure-functions-cli\bin"
+#r "Microsoft.Azure.WebJobs.Host.dll"
+#r "System.Web.Http.dll"
+#r "System.Net.Http.dll"
+#r "Newtonsoft.Json.dll"
+#r "System.Net.Http.Formatting.dll"
+#r "System.Net.Http.Extensions.dll"
+#endif
+
+
+#r "System.Configuration"
 #r "System.Net.Http"
 #r "Newtonsoft.Json"
 
+open System
+open System.Configuration
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+
+#r "Microsoft.WindowsAzure.Storage"
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Blob
 
 type Named = {
     name: string
 }
 
-let Run(req: HttpRequestMessage, log: TraceWriter) =
+let Run(req: HttpRequestMessage, id: string, log: TraceWriter) =
     async {
-        log.Info(sprintf 
-            "F# HTTP trigger function processed a request.")
+        
+        // Retrieve storage account from connection string.
+        let storageAccount = 
+                    CloudStorageAccount.Parse(ConfigurationManager.AppSettings.["AzureWebJobsStorage"])
+        
+        // Create the blob client.
+        let blobClient = storageAccount.CreateCloudBlobClient();
 
+        // Retrieve reference to a previously created container.
+        let container = blobClient.GetContainerReference("games");
+        
+        container.CreateIfNotExists() |> ignore
 
-        // Set name to query string
+        // Retrieve reference to a blob named "myblob".
+        let blockBlob = container.GetBlockBlobReference(id);
+
+        let! blobContents = blockBlob.DownloadTextAsync() |> Async.AwaitTask
+
+        return req.CreateResponse(HttpStatusCode.OK, blobContents);
+
+(*        // Set name to query string
         let name =
             req.GetQueryNameValuePairs()
             |> Seq.tryFind (fun q -> q.Key = "name")
@@ -30,5 +67,5 @@ let Run(req: HttpRequestMessage, log: TraceWriter) =
                 let named = JsonConvert.DeserializeObject<Named>(data)
                 return req.CreateResponse(HttpStatusCode.OK, "Hello " + named.name);
             else
-                return req.CreateResponse(HttpStatusCode.BadRequest, "Specify a Name value");
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Specify a Name value");*)
     } |> Async.RunSynchronously
---
+++ StartGame/run.csx
@@ -0,0 +1,42 @@
+//#if !COMPILED
+//#I @"C:\Users\aidan.treasure\AppData\Roaming\npm\node_modules\azure-functions-cli\bin"
+// #r "Microsoft.Azure.WebJobs.Host.dll"
+// #r "System.Web.Http.dll"
+// #r "System.Net.Http.dll"
+// #r "Newtonsoft.Json.dll"
+// #r "System.Net.Http.Formatting.dll"
+// #r "System.Net.Http.Extensions.dll"
+#r "Microsoft.WindowsAzure.Storage"
+#r "System.Configuration"
+//#endif
+
+using System;
+using System.Net;
+using System.Configuration;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, string name, TraceWriter log)
+{    
+    log.Info("C# HTTP trigger function processed a request.");
+    
+    // Retrieve storage account from connection string.
+    CloudStorageAccount storageAccount =
+            CloudStorageAccount.Parse(ConfigurationManager.AppSettings["AzureWebJobsStorage"]);
+
+    // Create the blob client.
+    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+    // Retrieve reference to a previously created container.
+    CloudBlobContainer container = blobClient.GetContainerReference("games");
+    container.CreateIfNotExists();
+    
+    var gameGuid = Guid.NewGuid();
+    
+    CloudBlockBlob blockBlob = container.GetBlockBlobReference(gameGuid);
+    blockBlob.UploadText("{ state: Started }");
+
+    // Fetching the name from the path parameter in the request URL
+    return req.CreateResponse(HttpStatusCode.OK, "Hello " + name + gameGuid);
+}
